7c824451e8e30529a8eaa630f120a6a652cfc711
iamdramaticleo <iamdramaticleo@gmail.com>
2025-10-01 21:40:56 +0300

    Fix minor formatting inconsistency in OpenGL Buffer declarations

--- opengl/buffer.hpp
+++ opengl/buffer.hpp
@@ -7,8 +7,8 @@ namespace opengl
     class Buffer final : public Object
     {
     public:
-        auto  create() -> void override;
-        auto destroy() -> void override;
+        auto  create() ->  void override;
+        auto destroy() ->  void override;
 
         auto storage(const core::buffer::data& data, uint32_t flags = 0) const -> void;
     };
