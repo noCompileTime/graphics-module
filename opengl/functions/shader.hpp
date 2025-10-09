5f1468abd51a5616831521839036ae5e4de86db4
iamdramaticleo <iamdramaticleo@gmail.com>
2025-10-10 00:19:50 +0300

    Fix alignment inconsistencies in OpenGL shader function pointer declarations

--- opengl/functions/shader.hpp
+++ opengl/functions/shader.hpp
@@ -8,8 +8,8 @@ namespace opengl::functions
     using  PFNGLSHADERBINARYPROC     = auto(APIENTRY*)(GLsizei count, const GLuint* shaders, GLenum format, const void* binary, GLsizei length)                              -> void;
     using  PFNGLSPECIALIZESHADERPROC = auto(APIENTRY*)(GLuint shader, const char* entry_point, GLuint constants, const GLuint* constant_index, const GLuint* constant_value) -> void;
 
-    inline PFNGLCREATESHADERPROC     create_shader;
-    inline PFNGLDELETESHADERPROC     delete_shader;
+    inline PFNGLCREATESHADERPROC         create_shader;
+    inline PFNGLDELETESHADERPROC         delete_shader;
 
     inline PFNGLSPECIALIZESHADERPROC specialize_shader;
     inline PFNGLSHADERBINARYPROC         shader_binary;
