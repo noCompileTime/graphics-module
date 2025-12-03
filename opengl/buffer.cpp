a19b31c6a81a275e822a428c587d0691352c0942
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-03 03:06:47 +0200

    Refactor Buffer class to replace bind_base with bind
    Replaced the `bind_base` method with a renamed and consistent `bind` method across the Buffer class. Updated corresponding method calls in client code and adjusted formatting for improved readability. Removed unused code and ensured functionality remains intact.

--- opengl/buffer.cpp
+++ opengl/buffer.cpp
@@ -12,11 +12,6 @@ namespace opengl
         functions::delete_buffers(1, &_handle);
     }
 
-    auto Buffer::bind_base(const uint32_t target, const uint32_t location) const -> void
-    {
-        functions::bind_buffer_base(target, location, _handle);
-    }
-
     auto Buffer::storage(const core::buffer::data& data, const uint32_t usage) const -> void
     {
         functions::buffer_storage(_handle, data.size, data.ptr, usage);
@@ -25,5 +20,10 @@ namespace opengl
     auto Buffer::upload(const core::buffer::data& data, const uint32_t offset) const -> void
     {
         functions::buffer_update(_handle, offset, data.size, data.ptr);
+    }
+
+    auto Buffer::bind(const uint32_t target, const uint32_t location) const -> void
+    {
+        functions::bind_buffer_base(target, location, _handle);
     }
 }
--- opengl/buffer.hpp
+++ opengl/buffer.hpp
@@ -7,12 +7,12 @@ namespace opengl
     class Buffer final : public Object
     {
     public:
-        auto    create() ->  void override;
-        auto   destroy() ->  void override;
+        auto  create() ->  void override;
+        auto destroy() ->  void override;
 
-        auto bind_base(uint32_t target, uint32_t location)              const -> void;
+        auto storage(const core::buffer::data& data, uint32_t  usage) const -> void;
+        auto  upload(const core::buffer::data& data, uint32_t offset) const -> void;
 
-        auto   storage(const core::buffer::data& data, uint32_t  usage) const -> void;
-        auto    upload(const core::buffer::data& data, uint32_t offset) const -> void;
+        auto    bind(uint32_t target, uint32_t location)              const -> void;
     };
 }
