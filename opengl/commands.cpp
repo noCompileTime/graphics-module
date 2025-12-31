4739a879267dc61c610697b313479da8905aa3dc
iamdramaticleo <iamdramaticleo@gmail.com>
2025-12-31 18:59:56 +0200

    Mark OpenGL `Commands` methods as `noexcept` for improved exception safety

--- opengl/commands.cpp
+++ opengl/commands.cpp
@@ -2,27 +2,27 @@
 
 namespace opengl
 {
-    auto Commands::clear(const uint32_t flags) -> void
+    auto Commands::clear(const uint32_t flags) noexcept -> void
     {
         functions::glClear(flags);
     }
 
-    auto Commands::clear(const float r, const float g, const float b, const float a) -> void
+    auto Commands::clear(const float r, const float g, const float b, const float a) noexcept -> void
     {
         functions::glClearColor(r, g, b, a);
     }
 
-    auto Commands::draw_vertices(const uint32_t primitive, const int32_t count, const int32_t offset) -> void
+    auto Commands::draw_vertices(const uint32_t primitive, const int32_t count, const int32_t offset) noexcept -> void
     {
         functions::glDrawArrays(primitive, offset, count);
     }
 
-    auto Commands::draw_elements(const uint32_t primitive, const int32_t count, const int32_t offset) -> void
+    auto Commands::draw_elements(const uint32_t primitive, const int32_t count, const int32_t offset) noexcept -> void
     {
         functions::glDrawElements(primitive, count, constants::uint32_type, reinterpret_cast<const void*>(offset * sizeof(uint32_t)));
     }
 
-    auto Commands::viewport(const int32_t x, const int32_t y, const int32_t width, const int32_t height) -> void
+    auto Commands::viewport(const int32_t x, const int32_t y, const int32_t width, const int32_t height) noexcept -> void
     {
         functions::glViewport(x, y, width, height);
     }
