f5c8850b0a357a4a8800c3afb2f4aeec30bb9faa
iamdramaticleo <iamdramaticleo@gmail.com>
2026-01-11 17:19:31 +0200

    Simplify buffer storage function by removing explicit usage.
    Updated the `opengl::Buffer::storage` function to use a default value for the `usage` parameter, reducing redundancy in function calls. Several instances in `main.cpp` were updated accordingly for consistency and clarity.

--- opengl/buffer.hpp
+++ opengl/buffer.hpp
@@ -10,8 +10,8 @@ namespace opengl
         auto  create() noexcept -> void override;
         auto destroy() noexcept -> void override;
 
-        auto storage(const core::data::buffer& data, uint32_t  usage) const noexcept -> void;
-        auto  upload(const core::data::buffer& data, uint32_t offset) const noexcept -> void;
+        auto storage(const core::data::buffer& data, uint32_t usage = 0) const noexcept -> void;
+        auto  upload(const core::data::buffer& data, uint32_t offset)    const noexcept -> void;
 
         auto    bind(uint32_t target, uint32_t index) const noexcept -> void;
     };
