12fb74bae9c2afad88a4ad5d900475089693015e
iamdramaticleo <iamdramaticleo@gmail.com>
2026-01-24 19:48:27 +0200

    Overload `Buffer::storage` method for buffer allocation without data delivery

--- opengl/buffer.hpp
+++ opengl/buffer.hpp
@@ -10,9 +10,11 @@ namespace opengl
         auto  create() noexcept -> void override;
         auto destroy() noexcept -> void override;
 
-        auto storage(uint32_t buffer_size,             uint32_t usage  = constants::default_usage)        noexcept -> void;
         auto storage(const core::data::buffer& buffer, uint32_t usage  = constants::default_usage)        noexcept -> void;
         auto  upload(const core::data::buffer& buffer, uint32_t offset = constants::default_offset) const noexcept -> void;
+
+        //   used when you want to allocate the buffer without actually delivering the data
+        auto storage(uint32_t buffer_size, uint32_t usage = constants::default_usage) noexcept -> void;
 
         auto  bind(uint32_t target, uint32_t index) const noexcept -> void;
         auto  bind(uint32_t target)                 const noexcept -> void;
