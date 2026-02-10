2ab6a29b67fe063e64edc20ec4a786f9ee03698e
iamdramaticleo <iamdramaticleo@gmail.com>
2026-02-10 23:54:09 +0200

    Fix spacing in `storage` method signature for consistency

--- opengl/buffer.hpp
+++ opengl/buffer.hpp
@@ -10,7 +10,7 @@ namespace opengl
         auto  create() noexcept -> void override;
         auto destroy() noexcept -> void override;
 
-        auto storage(const core::data::buffer& buffer, uint32_t  usage)       noexcept -> void;
+        auto storage(const core::data::buffer& buffer, uint32_t usage)        noexcept -> void;
         auto  upload(const core::data::buffer& buffer, uint32_t offset) const noexcept -> void;
 
         //   used when you want to allocate the buffer but not actually send the data up front
