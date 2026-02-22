92d3ba805b75847dbe375d61570df780078defc9
iamdramaticleo <iamdramaticleo@gmail.com>
2026-02-22 21:05:21 +0200

    Add `glPolygonMode` function bindings and fix spacing in method definitions for consistency

--- opengl/functions/pipeline.hpp
+++ opengl/functions/pipeline.hpp
@@ -14,6 +14,8 @@ namespace opengl::functions
     using  PFNGLSTENCILFUNCPROC = auto(APIENTRY*)(uint32_t func,  int32_t ref,   uint32_t mask)  -> void;
     using  PFNGLSTENCILOPPROC   = auto(APIENTRY*)(uint32_t fail, uint32_t zfail, uint32_t zpass) -> void;
 
+    using  PFNGLPOLYGONMODEPROC = auto(APIENTRY*)(uint32_t face, uint32_t mode) -> void;
+
     using  PFNGLENABLEPROC      = auto(APIENTRY*)(uint32_t cap)  -> void;
     using  PFNGLDISABLEPROC     = auto(APIENTRY*)(uint32_t cap)  -> void;
 
@@ -23,6 +25,8 @@ namespace opengl::functions
     inline PFNGLSTENCILMASKPROC glStencilMask;
     inline PFNGLSTENCILFUNCPROC glStencilFunc;
     inline PFNGLSTENCILOPPROC   glStencilOp;
+
+    inline PFNGLPOLYGONMODEPROC glPolygonMode;
 
     inline PFNGLENABLEPROC      glEnable;
     inline PFNGLDISABLEPROC     glDisable;
