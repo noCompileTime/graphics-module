f54a99f88f092f4dd8559132877a412592573833
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-09 17:14:32 +0200

    Add `bind` method declaration to `vertex_array.hpp`

--- opengl/vertex_array.hpp
+++ opengl/vertex_array.hpp
@@ -14,6 +14,7 @@ namespace opengl
         auto attach_elements(const Buffer& buffer)                 const noexcept -> void;
 
         auto attach(const core::vertex::attribute& attribute)      const noexcept -> void;
+
         auto bind() const noexcept -> void;
     };
 }
