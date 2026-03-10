9f885492a0d43fd5378095508af6dd3d46f138af
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-10 16:32:25 +0200

    Refactor texture upload to use uint8_t instead of std::byte
    Updated the `upload` method in `Texture` to use `std::span<const uint8_t>` for better alignment with pixel data. Adjusted relevant calls to remove unnecessary `as_bytes` conversion.

--- opengl/texture.cpp
+++ opengl/texture.cpp
@@ -22,7 +22,7 @@ namespace opengl
         functions::glTextureStorage2D(_handle, levels, format, width, height);
     }
 
-    auto Texture::upload(const int32_t width, const int32_t height, const uint32_t format, const int32_t level, const std::span<const std::byte> buffer) const noexcept -> void
+    auto Texture::upload(const int32_t width, const int32_t height, const uint32_t format, const int32_t level, const std::span<const uint8_t> buffer) const noexcept -> void
     {
         functions::glTextureSubImage2D(_handle, level, 0, 0, width, height, format, constants::unsigned_byte_type, buffer.data());
     }
