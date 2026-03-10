356f5bdd368788cc8a2930c73cc52484a6563e41
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-10 11:30:32 +0200

    Fix indentation in `shader_stage.hpp` for consistency

--- opengl/shader_stage.hpp
+++ opengl/shader_stage.hpp
@@ -7,11 +7,10 @@ namespace opengl
     class ShaderStage final : public Object
     {
     public:
+        explicit ShaderStage(uint32_t type);
 
-    explicit ShaderStage(uint32_t type);
-
-        auto  create() noexcept -> void override;
-        auto destroy() noexcept -> void override;
+        auto create()  noexcept ->  void override;
+        auto destroy() noexcept ->  void override;
 
         auto source(std::span<const std::byte> source) const noexcept -> void;
     };
