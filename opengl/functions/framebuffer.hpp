b32e25d1e6f478605d3d8dff67d9f501eeb0e6dd
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-11 17:51:32 +0200

    Fix indentation in `framebuffer.hpp` for consistency

--- opengl/functions/framebuffer.hpp
+++ opengl/functions/framebuffer.hpp
@@ -2,21 +2,21 @@
 
 namespace opengl::functions
 {
-    using  PFNGLCREATEFRAMEBUFFERSPROC      = void(APIENTRY*)(int32_t n,       uint32_t* framebuffers);
-    inline PFNGLCREATEFRAMEBUFFERSPROC      glCreateFramebuffers;
+     using PFNGLCREATEFRAMEBUFFERSPROC = void(APIENTRY*)(int32_t n, uint32_t* framebuffers);
+    inline PFNGLCREATEFRAMEBUFFERSPROC glCreateFramebuffers;
 
-    using  PFNGLDELETEFRAMEBUFFERSPROC      = void(APIENTRY*)(int32_t n, const uint32_t* framebuffers);
-    inline PFNGLDELETEFRAMEBUFFERSPROC      glDeleteFramebuffers;
+     using PFNGLDELETEFRAMEBUFFERSPROC = void(APIENTRY*)(int32_t n, const uint32_t* framebuffers);
+    inline PFNGLDELETEFRAMEBUFFERSPROC glDeleteFramebuffers;
 
-    using  PFNGLBINDFRAMEBUFFERPROC         = void(APIENTRY*)(uint32_t target, uint32_t  framebuffer);
-    inline PFNGLBINDFRAMEBUFFERPROC         glBindFramebuffer;
+     using PFNGLBINDFRAMEBUFFERPROC = void(APIENTRY*)(uint32_t target, uint32_t  framebuffer);
+    inline PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer;
 
-    using  PFNGLFRAMEBUFFERTEXTUREPROC      = void(APIENTRY*)(uint32_t framebuffer, uint32_t attachment, uint32_t texture, int32_t level);
-    inline PFNGLFRAMEBUFFERTEXTUREPROC      glFramebufferTexture;
+     using PFNGLFRAMEBUFFERTEXTUREPROC = void(APIENTRY*)(uint32_t framebuffer, uint32_t attachment, uint32_t texture, int32_t level);
+    inline PFNGLFRAMEBUFFERTEXTUREPROC glFramebufferTexture;
 
-    using  PFNGLFRAMEBUFFERRENDERBUFFERPROC = void(APIENTRY*)(uint32_t framebuffer, uint32_t attachment, uint32_t target, uint32_t renderbuffer);
+     using PFNGLFRAMEBUFFERRENDERBUFFERPROC = void(APIENTRY*)(uint32_t framebuffer, uint32_t attachment, uint32_t target, uint32_t renderbuffer);
     inline PFNGLFRAMEBUFFERRENDERBUFFERPROC glFramebufferRenderbuffer;
 
-    using  PFNGLCHECKFRAMEBUFFERSTATUSPROC  = uint32_t(APIENTRY*)(uint32_t framebuffer, uint32_t target);
-    inline PFNGLCHECKFRAMEBUFFERSTATUSPROC  glCheckFramebufferStatus;
+     using PFNGLCHECKFRAMEBUFFERSTATUSPROC = uint32_t(APIENTRY*)(uint32_t framebuffer, uint32_t target);
+    inline PFNGLCHECKFRAMEBUFFERSTATUSPROC glCheckFramebufferStatus;
 }
