3eca21e3a096ffec2c704ed326892a58716a5bde
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-18 21:47:02 +0200

    Add `PipelineDebug` class and integrate it into OpenGL module

--- opengl/pipeline_debug.cpp
+++ opengl/pipeline_debug.cpp
@@ -2,4 +2,24 @@
 
 namespace opengl
 {
+    auto PipelineDebug::init() noexcept -> void
+    {
+        functions::glEnable(constants::debug_output);
+        //functions::glEnable(constants::debug_output_synchronous);
+
+        //functions::glDebugMessageControl(constants::dont_care, constants::dont_care, constants::debug_severity_notification, 0, nullptr, false);
+
+        functions::glDebugMessageCallback([](uint32_t /* source   */,
+                                             uint32_t /* type     */,
+                                             uint32_t /* id       */,
+                                             uint32_t /* severity */, int32_t /* length */, const char* message, const void* /* param */) {
+            std::println("{}", message);
+
+        }, nullptr);
+    }
+
+    auto PipelineDebug::polygon_mode(const uint32_t mode) noexcept -> void
+    {
+        functions::glPolygonMode(constants::front_and_back, mode);
+    }
 }
--- opengl/pipeline_debug.hpp
+++ opengl/pipeline_debug.hpp
@@ -4,5 +4,11 @@ namespace opengl
 {
     class PipelineDebug
     {
+    public:
+        PipelineDebug() = delete;
+
+        static auto init() noexcept -> void;
+
+        static auto polygon_mode(uint32_t mode) noexcept -> void;
     };
 }
