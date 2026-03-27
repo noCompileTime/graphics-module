ecafa830d758a0e6aada78698aeef04c5881ea35
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-27 09:51:02 +0200

    Format constants and comment out unused module.
    Aligned constant definitions in `pipeline.hpp` for better readability. Commented out the `tools` module directory in `CMakeLists.txt` to disable its inclusion for now.

--- opengl/constants/pipeline.hpp
+++ opengl/constants/pipeline.hpp
@@ -11,9 +11,9 @@ namespace opengl::constants
     constexpr auto   depth_test = 0x0B71;
     constexpr auto stencil_test = 0x0B90;
 
-    constexpr auto always     = 0x0207;
-    constexpr auto replace    = 0x1E01;
-    constexpr auto keep       = 0x1E00;
-    constexpr auto not_equal  = 0x0205;
-    constexpr auto less       = 0x0201;
+    constexpr auto always       = 0x0207;
+    constexpr auto replace      = 0x1E01;
+    constexpr auto keep         = 0x1E00;
+    constexpr auto not_equal    = 0x0205;
+    constexpr auto less         = 0x0201;
 }
