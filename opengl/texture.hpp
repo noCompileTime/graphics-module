6001f65ea266f1f23bea4c037045735492f778d7
iamdramaticleo <iamdramaticleo@gmail.com>
2026-03-29 15:55:37 +0300

    Fix indentation in `shader_stage.hpp` and `texture.hpp` for consistency

--- opengl/texture.hpp
+++ opengl/texture.hpp
@@ -9,12 +9,12 @@ namespace opengl
     public:
         explicit Texture(uint32_t target);
 
-            auto  create() noexcept -> void override;
-            auto destroy() noexcept -> void override;
+        auto  create() noexcept -> void override;
+        auto destroy() noexcept -> void override;
 
-            auto storage(int32_t width, int32_t height, uint32_t format, int32_t levels)                                 const noexcept -> void;
-            auto  upload(int32_t width, int32_t height, uint32_t format, int32_t level, std::span<const uint8_t> buffer) const noexcept -> void;
+        auto storage(int32_t width, int32_t height, uint32_t format, int32_t levels)                                 const noexcept -> void;
+        auto  upload(int32_t width, int32_t height, uint32_t format, int32_t level, std::span<const uint8_t> buffer) const noexcept -> void;
 
-            auto bind(uint32_t index) const noexcept -> void;
+        auto bind(uint32_t index) const noexcept -> void;
     };
 }
