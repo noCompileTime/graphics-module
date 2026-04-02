ed0085200116797ee45029e56bd3342583e01170
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-02 17:59:46 +0300

    Refactor includes and update type aliases for consistency
    Removed unused `core/headers.hpp`, adjusted OpenGL type aliases to use `std::` types, and reorganized include statements for better clarity. Also updated CMake file by removing precompiled headers configuration for `core` module.

--- opengl/functions/texture.hpp
+++ opengl/functions/texture.hpp
@@ -2,18 +2,18 @@
 
 namespace opengl::functions
 {
-     using PFNGLCREATETEXTURESPROC = void(APIENTRY*)(uint32_t target, int32_t n, uint32_t* textures);
+     using PFNGLCREATETEXTURESPROC = void(APIENTRY*)(std::uint32_t target, std::int32_t n, std::uint32_t* textures);
     inline PFNGLCREATETEXTURESPROC glCreateTextures;
 
-     using PFNGLDELETETEXTURESPROC = void(APIENTRY*)(int32_t n, const uint32_t* textures);
+     using PFNGLDELETETEXTURESPROC = void(APIENTRY*)(std::int32_t n, const std::uint32_t* textures);
     inline PFNGLDELETETEXTURESPROC glDeleteTextures;
 
-     using PFNGLTEXTURESTORAGE2DPROC = void(APIENTRY*)(uint32_t texture, int32_t levels, uint32_t format, int32_t width, int32_t height);
+     using PFNGLTEXTURESTORAGE2DPROC = void(APIENTRY*)(std::uint32_t texture, std::int32_t levels, std::uint32_t format, std::int32_t width, std::int32_t height);
     inline PFNGLTEXTURESTORAGE2DPROC glTextureStorage2D;
 
-     using PFNGLTEXTURESUBIMAGE2DPROC = void(APIENTRY*)(uint32_t texture, int32_t level, int32_t xoffset, int32_t yoffset, int32_t width, int32_t height, uint32_t format, uint32_t type, const void* pixels);
+     using PFNGLTEXTURESUBIMAGE2DPROC = void(APIENTRY*)(std::uint32_t texture, std::int32_t level, int32_t xoffset, int32_t yoffset, int32_t width, int32_t height, uint32_t format, uint32_t type, const void* pixels);
     inline PFNGLTEXTURESUBIMAGE2DPROC glTextureSubImage2D;
 
-     using PFNGLBINDTEXTUREUNITPROC = void(APIENTRY*)(uint32_t unit, uint32_t texture);
+     using PFNGLBINDTEXTUREUNITPROC = void(APIENTRY*)(std::uint32_t unit, std::uint32_t texture);
     inline PFNGLBINDTEXTUREUNITPROC glBindTextureUnit;
 }
--- opengl/headers.hpp
+++ opengl/headers.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
+
 #include <windows.h>
 
 #include "functions/buffer.hpp"
