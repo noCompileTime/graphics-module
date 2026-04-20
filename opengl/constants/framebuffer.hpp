0f4a8e6fc74e5722a8013a10ed4e86fef497bfab
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-20 23:27:05 +0300

    Ensure consistent naming and formatting in OpenGL framebuffer constants

--- opengl/constants/framebuffer.hpp
+++ opengl/constants/framebuffer.hpp
@@ -2,11 +2,11 @@
 
 namespace opengl::constants
 {
-    constexpr auto         color_attachment0 = 0x8CE0;
-    constexpr auto         depth_attachment  = 0x8D00;
-    constexpr auto       stencil_attachment  = 0x8D20;
-    constexpr auto depth_stencil_attachment  = 0x821A;
+    constexpr auto         color_attachment_0 = 0x8CE0;
+    constexpr auto         depth_attachment   = 0x8D00;
+    constexpr auto       stencil_attachment   = 0x8D20;
+    constexpr auto depth_stencil_attachment   = 0x821A;
 
-    constexpr auto   framebuffer_complete    = 0x8CD5;
-    constexpr auto   framebuffer             = 0x8D40;
+    constexpr auto   framebuffer_complete     = 0x8CD5;
+    constexpr auto   framebuffer              = 0x8D40;
 }
