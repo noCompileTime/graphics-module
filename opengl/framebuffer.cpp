0033a61d63233c7a5cebff0ae5eb39827df7ffe8
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-23 00:05:50 +0300

    Add framebuffer support and integrate off-screen rendering
    Introduced the `Framebuffer` class with methods for creation, destruction, attachment, binding, and status checks. Integrated an off-screen rendering pipeline with a secondary framebuffer for rendering the game view texture, which is then displayed onto the default framebuffer. Updated VAO, VBO, and UBO handling to support the new rendering workflow.

--- opengl/framebuffer.cpp
+++ opengl/framebuffer.cpp
@@ -2,4 +2,28 @@
 
 namespace opengl
 {
+    auto Framebuffer::create() noexcept -> void
+    {
+        functions::glCreateFramebuffers(1, &_handle);
+    }
+
+    auto Framebuffer::destroy() noexcept -> void
+    {
+        functions::glDeleteFramebuffers(1, &_handle);
+    }
+
+    auto Framebuffer::attach(const Texture& texture, const uint32_t attachment, const uint32_t level) const noexcept -> void
+    {
+        functions::glFramebufferTexture(_handle, attachment, texture.handle(), level);
+    }
+
+    auto Framebuffer::status() const noexcept -> uint32_t
+    {
+        return functions::glCheckFramebufferStatus(_handle, constants::framebuffer);
+    }
+
+    auto Framebuffer::bind() const noexcept -> void
+    {
+        functions::glBindFramebuffer(constants::framebuffer, _handle);
+    }
 }
--- opengl/framebuffer.hpp
+++ opengl/framebuffer.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
+#include "texture.hpp"
+
 namespace opengl
 {
-    class Framebuffer
+    class Framebuffer final : public Object
     {
+    public:
+        auto  create() noexcept -> void override;
+        auto destroy() noexcept -> void override;
+
+        auto attach(const Texture& texture, uint32_t attachment, uint32_t level) const noexcept -> void;
+
+        auto status() const noexcept -> uint32_t;
+        auto   bind() const noexcept -> void;
+
+        /* TODO implement this instead of glClearColor or glClear
+
+        glClearNamedFramebufferfv(fb, GL_COLOR, 0, rgba);
+        glClearNamedFramebufferfv(fb, GL_DEPTH, 0, &depth);
+
+         */
     };
 }
