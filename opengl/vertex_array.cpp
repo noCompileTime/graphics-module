473266bb0f606c4eb216467bd7a1a41239efc236
iamdramaticleo <iamdramaticleo@gmail.com>
2026-04-30 10:29:06 +0300

    Refactor vertex attribute structure and VAO attachment.
    Reorganized `attribute` structure by renaming `offset` to `start` and adjusting its order. Updated all VAO attachment calls to reflect the new attribute structure for improved clarity and consistency. Adjusted `glVertexArrayAttribFormat` usage to accommodate these changes.

--- opengl/vertex_array.cpp
+++ opengl/vertex_array.cpp
@@ -24,7 +24,7 @@ namespace opengl
 
     auto VertexArray::attach(const core::vertex::attribute& attribute) const noexcept -> void
     {
-        functions::glVertexArrayAttribFormat (_handle, attribute.index, attribute.size, attribute.type, 0, attribute.offset);
+        functions::glVertexArrayAttribFormat (_handle, attribute.index, attribute.size, attribute.type, 0, attribute.start);
         functions::glVertexArrayAttribBinding(_handle, attribute.index, 0); // TODO add the binding to attribute
 
         functions::glEnableVertexArrayAttrib (_handle, attribute.index);
