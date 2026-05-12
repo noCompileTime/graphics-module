a9a40f1d2169ce5e8fd0c322b55292cd603e1b69
iamdramaticleo <iamdramaticleo@gmail.com>
2026-05-12 23:50:20 +0300

    Add texture wrapping and clamping constants; align formatting in pipeline constants

--- opengl/constants/pipeline.hpp
+++ opengl/constants/pipeline.hpp
@@ -7,9 +7,9 @@ namespace opengl::constants
     constexpr auto one_minus_src_alpha = 0x0303;
     constexpr auto           src_alpha = 0x0302;
 
-    constexpr auto   blend_mode = 0x0BE2;
     constexpr auto    fill_mode = 0x1B02;
     constexpr auto    line_mode = 0x1B01;
+    constexpr auto   blend_mode = 0x0BE2;
 
     constexpr auto    cull_test = 0x0B44;
     constexpr auto   depth_test = 0x0B71;
@@ -17,9 +17,9 @@ namespace opengl::constants
 
     constexpr auto  multisample = 0x809D;
 
-    constexpr auto  always     = 0x0207;
-    constexpr auto  replace    = 0x1E01;
-    constexpr auto  keep       = 0x1E00;
-    constexpr auto  not_equal  = 0x0205;
-    constexpr auto  less       = 0x0201;
+    constexpr auto  always      = 0x0207;
+    constexpr auto  replace     = 0x1E01;
+    constexpr auto  keep        = 0x1E00;
+    constexpr auto  not_equal   = 0x0205;
+    constexpr auto  less        = 0x0201;
 }
--- opengl/constants/sampler.hpp
+++ opengl/constants/sampler.hpp
@@ -2,6 +2,11 @@
 
 namespace opengl::constants
 {
+    constexpr auto texture_wrap_s = 0x2802;
+    constexpr auto texture_wrap_t = 0x2803;
+
+    constexpr auto  clamp_to_edge = 0x812F;
+
     constexpr auto min_filter = 0x2801;
     constexpr auto mag_filter = 0x2800;
 
