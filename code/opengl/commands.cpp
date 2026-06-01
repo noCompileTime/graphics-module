77a85caa456c4dbeeed9d1e717af2660d68bdf5c
iamdramaticleo <iamdramaticleo@gmail.com>
2026-06-01 22:14:29 +0300

    Reorganize and reimplement OpenGL `clear` methods
    Moved and redefined `clear` methods in OpenGL commands for better structure and readability. Updated the order in the header file and re-added their implementation in the source file. Minor formatting adjustments were also made for consistency.

--- code/opengl/commands.cpp
+++ code/opengl/commands.cpp
@@ -2,16 +2,6 @@
 
 namespace opengl
 {
-    auto Commands::clear(const uint32_t flags) noexcept -> void
-    {
-        functions::glClear(flags);
-    }
-
-    auto Commands::clear(const float r, const float g, const float b, const float a) noexcept -> void
-    {
-        functions::glClearColor(r, g, b, a);
-    }
-
     auto Commands::draw_vertices(const uint32_t primitive, const int32_t start, const int32_t count) noexcept -> void
     {
         functions::glDrawArrays(primitive, start, count);
@@ -20,11 +10,21 @@ namespace opengl
     auto Commands::draw_elements(const uint32_t primitive, const int32_t start, const int32_t count) noexcept -> void
     {
         functions::glDrawElements(primitive, count, constants::unsigned_int,
-            reinterpret_cast<const void*>(static_cast<uintptr_t>(start) * sizeof(uint32_t)));
+                                  reinterpret_cast<const void*>(static_cast<uintptr_t>(start) * sizeof(uint32_t)));
     }
 
     auto Commands::viewport(const int32_t x, const int32_t y, const int32_t width, const int32_t height) noexcept -> void
     {
         functions::glViewport(x, y, width, height);
+    }
+
+    auto Commands::clear(const float r, const float g, const float b, const float a) noexcept -> void
+    {
+        functions::glClearColor(r, g, b, a);
+    }
+
+    auto Commands::clear(const uint32_t flags) noexcept -> void
+    {
+        functions::glClear(flags);
     }
 }
--- code/opengl/commands.hpp
+++ code/opengl/commands.hpp
@@ -5,13 +5,13 @@ namespace opengl
     class Commands
     {
     public:
-        static auto  clear(uint32_t flags)                     noexcept -> void;
-        static auto  clear(float r, float g, float b, float a) noexcept -> void;
-
         static auto  draw_vertices(uint32_t primitive, int32_t start, int32_t count) noexcept -> void;
         static auto  draw_elements(uint32_t primitive, int32_t start, int32_t count) noexcept -> void;
 
-        static auto  viewport(int32_t x, int32_t y, int32_t width, int32_t height) noexcept -> void;
+        static auto  viewport(int32_t x, int32_t y, int32_t width, int32_t height)   noexcept -> void;
+
+        static auto  clear(float r, float g, float b, float a) noexcept -> void;
+        static auto  clear(uint32_t flags)                     noexcept -> void;
 
         Commands() = delete;
     };
