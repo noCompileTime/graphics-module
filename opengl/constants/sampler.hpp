66c4f320251cb45c15b3689847e504cb5b8778a9
iamdramaticleo <iamdramaticleo@gmail.com>
2026-06-10 20:43:52 +0300

    Update graphics module file

--- opengl/constants/sampler.hpp
+++ opengl/constants/sampler.hpp
@@ -2,14 +2,14 @@
 
 namespace opengl::constants
 {
+    constexpr auto min_filter = 0x2801;
+    constexpr auto mag_filter = 0x2800;
+
     constexpr auto wrap_s = 0x2802;
     constexpr auto wrap_t = 0x2803;
 
     constexpr auto clamp_to_edge = 0x812F;
 
-    constexpr auto min_filter = 0x2801;
-    constexpr auto mag_filter = 0x2800;
-
-    constexpr auto    nearest = 0x2600;
-    constexpr auto     linear = 0x2601;
+    constexpr auto nearest = 0x2600;
+    constexpr auto  linear = 0x2601;
 }
