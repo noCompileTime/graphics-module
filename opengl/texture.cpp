978c16cb57b7749db69f7a4fea5b6b0bbdccf708
iamdramaticleo <iamdramaticleo@gmail.com>
2026-06-19 22:37:03 +0300

    Reorder `Texture::storage` implementation for improved code structure and consistency.

--- opengl/texture.cpp
+++ opengl/texture.cpp
@@ -17,14 +17,14 @@ namespace opengl
         functions::glDeleteTextures(1, &_handle);
     }
 
-    auto Texture::storage(const int32_t width, const int32_t height, const uint32_t format, const int32_t levels) const noexcept -> void
-    {
-        functions::glTextureStorage2D(_handle, levels, format, width, height);
-    }
-
     auto Texture::upload(const int32_t x, const int32_t y, const int32_t width, const int32_t height, const uint32_t format, const int32_t level, const uint32_t type, const std::span<const uint8_t> buffer) const noexcept -> void
     {
         functions::glTextureSubImage2D(_handle, level, x, y, width, height, format, type, buffer.data()); // TODO buffer should be a span with std::byte, maybe without std::
+    }
+
+    auto Texture::storage(const int32_t width, const int32_t height, const uint32_t format, const int32_t levels) const noexcept -> void
+    {
+        functions::glTextureStorage2D(_handle, levels, format, width, height);
     }
 
     auto Texture::bind(const uint32_t index) const noexcept -> void
