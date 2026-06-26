c47aa0e5d68e6595e9218e366ba2bcea5171f103
iamdramaticleo <iamdramaticleo@gmail.com>
2026-06-26 16:15:09 +0300

    Refactor framebuffer clear method to use default level
    Updated the `clear` method in `Framebuffer` to make the level parameter optional with a default value of 0, simplifying usage. Adjusted relevant code in `main.cpp` to reflect this change.

--- opengl/framebuffer.hpp
+++ opengl/framebuffer.hpp
@@ -15,8 +15,8 @@ namespace opengl
         auto   attach(const Texture& texture,     uint32_t attachment, int32_t level) const noexcept -> void;
         auto   attach(const Renderbuffer& buffer, uint32_t attachment)                const noexcept -> void;
 
-        auto    clear(const float* color, int32_t level) const noexcept -> void;
-        auto    clear(float depth)                       const noexcept -> void;
+        auto    clear(const float* color, int32_t level = 0) const noexcept -> void; // TODO instead of zero create a default_level/layer to be used also for texture
+        auto    clear(float depth)                           const noexcept -> void;
 
         auto complete() const noexcept -> void;
         auto     bind() const noexcept -> void;
