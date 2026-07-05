353a2cb17ef0206f84f557d177080e86417583bd
iamdramaticleo <iamdramaticleo@gmail.com>
2026-07-05 20:25:45 +0300

    Add a note to `viewport` method suggesting a potential `Viewport` struct for reusability in other methods

--- opengl/commands.hpp
+++ opengl/commands.hpp
@@ -8,7 +8,7 @@ namespace opengl
         static auto draw_vertices(uint32_t primitive, int32_t start, int32_t count) noexcept -> void;
         static auto draw_elements(uint32_t primitive, int32_t start, int32_t count) noexcept -> void;
 
-        static auto viewport(int32_t x, int32_t y, int32_t width, int32_t height)   noexcept -> void;
+        static auto viewport(int32_t x, int32_t y, int32_t width, int32_t height)   noexcept -> void; // TODO create a viewport struct? to be used also in the read_pixels and other places?
 
         static auto clear(float r, float g, float b, float a) noexcept -> void;
         static auto clear(uint32_t flags)                     noexcept -> void;
