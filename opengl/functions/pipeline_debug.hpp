41c0e098ef1fa40e2a761669850a04a09a950052
iamdramaticleo <iamdramaticleo@gmail.com>
2026-08-06 01:28:54 +0300

    Standardize spacing in `GLDEBUGMESSAGECALLBACK` declaration for consistent formatting.

--- opengl/functions/pipeline_debug.hpp
+++ opengl/functions/pipeline_debug.hpp
@@ -2,8 +2,7 @@
 
 namespace opengl::functions
 {
-     using GLDEBUGMESSAGECALLBACK = void(APIENTRY*)(uint32_t source, uint32_t type, uint32_t id, uint32_t severity, int32_t length, const char* message, const void* param);
-
+     using    GLDEBUGMESSAGECALLBACK     = void(APIENTRY*)(uint32_t source, uint32_t type, uint32_t id, uint32_t severity, int32_t length, const char* message, const void* param);
      using PFNGLDEBUGMESSAGECALLBACKPROC = void(APIENTRY*)(GLDEBUGMESSAGECALLBACK callback, const void* param);
     inline PFNGLDEBUGMESSAGECALLBACKPROC glDebugMessageCallback;
 
