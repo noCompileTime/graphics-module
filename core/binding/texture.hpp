353d1f338d2c66e8620bc7b2cba4d5def8b030ae
iamdramaticleo <iamdramaticleo@gmail.com>
2026-08-17 00:25:54 +0300

    Add TODO comments in `texture.hpp` and `buffer.hpp` suggesting potential relocation to shader modules

--- core/binding/texture.hpp
+++ core/binding/texture.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-namespace core::binding
+namespace core::binding // TODO this should be in the shader modules? because of the bindings in the shaders
 {
     enum class texture : uint32_t // TODO named sampler?
     {
